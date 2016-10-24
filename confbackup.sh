6cb48a4fa87b43d38e1186c5824fc6f6de9351a5
Alexander Schneider <a.schneider@eckelmann.de>
2016-10-24 16:16:15 +0200

    First draft with traps

--- confbackup.sh
+++ confbackup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Description: Create a file based backup of the Confluence home and a database dump
+# Author: q4p14
 
 # Confluence variables
 CONFUSR=confluence
@@ -7,13 +9,42 @@ CONFHOME=/var/atlassian/application-data/confluence
 CONFDB=conf
 
 # Local variables
-TODAY=$(date +%a%_d-%m-%y_%H-%M-%S)
-TARGETDIR=/root
+TARGETDIR=/tmp
 LOGDIR=/var/log
+
+# Just get the date in the desired format (no symbols that must be escaped)
+function today {
+  date +%a%_d-%m-%y_%H-%M-%S
+}
+
+# This will be used to create a unique working directory
+TODAY=$(today)
+
+function cleanup {
+  echo "$(today): Recieved signal to stop, cleaning backup direcotry structure" >> $LOGDIR/confbackup.log
+  rm -rf $TARGETDIR/confbackup/$TODAY
+  su -c "$CONFDIR/bin/startup.sh" $CONFUSR
+
+  if [ ! -e "$CONFDIR/work/catalina.pid" ]; then
+    echo "$(today): Can't restart Confluence process" >> $LOGDIR/confbackup.log
+  fi
+}
+
+#Create the working directory for the backup, if it doesn't already exists
+if [ ! -e $TARGETDIR/confbackup/$TODAY ]; then
+        mkdir -p $TARGETDIR/confbackup/$TODAY/db
+        chown postgres:postgres $TARGETDIR/confbackup/$TODAY/db
+      else
+        echo "$(today): Can't create backup directory $TARGETDIR/confbackup/$TODAY, does it already exist?" >> $LOGDIR/confbackup.log
+        exit
+fi
+trap cleanup SIGTERM SIGHUP SIGINT
 
 # Shutdown the Confluence service and wait as the service is pretty unresponsive
 su -c "$CONFDIR/bin/shutdown.sh" $CONFUSR
+trap cleanup SIGTERM SIGHUP SIGINT
 sleep 1m
+trap cleanup SIGTERM SIGHUP SIGINT
 
 # If the Conluence service didn't shutdown properly, try the catalina stop script, which
 # kills the Tomcat process
@@ -21,6 +52,7 @@ if [ -e "$CONFDIR/work/catalina.pid" ]; then
         su -c "$CONFDIR/bin/catalina.sh stop -force" $CONFUSR
         sleep 1m
 fi
+trap cleanup SIGTERM SIGHUP SIGINT
 
 # If Confluenced isn't running anymore, backup the Confluence home directory:
 # 1. Saving the Confluence home directory.
@@ -29,18 +61,14 @@ fi
 # 3. Appending the existing archive and compressing it
 
 if [ ! -e "$CONFDIR/work/catalina.pid" ]; then
-        tar -cf "$TARGETDIR/conf_$TODAY.tar" $CONFHOME
-
-        mkdir /tmp/$TODAY
-        chown postgres:postgres /tmp/$TODAY
-        su -c "pg_dump conf > /tmp/$TODAY/conf_dump.sql" postgres
-
-        tar -rf "$TARGETDIR/conf_$TODAY.tar" /tmp/$TODAY/conf_dump.sql
-        gzip -9 "$TARGETDIR/conf_$TODAY.tar"
-
+        tar -cf "$TARGETDIR/confbackup/$TODAY/conf.tar" $CONFHOME
+        su -c "pg_dump conf > $TARGETDIR/confbackup//$TODAY/db/conf_dump.sql" postgres
+        tar -rf "$TARGETDIR/confbackup/$TODAY/conf.tar" $TARGETDIR/confbackup/$TODAY/db/conf_dump.sql
+        gzip -9 "$TARGETDIR/confbackup/$TODAY/conf.tar"
       else
-        echo "$(date +%a%_d-%m-%y_%H-%M-%S): Couldn't stop Confluence/Tomcat process" >> $LOGDIR/confbackup.log
+        echo "$(today): Couldn't stop Confluence/Tomcat process" >> $LOGDIR/confbackup.log
 fi
+trap cleanup SIGTERM SIGHUP SIGINT
 
 # Restart Tomcat and the Confluence service
 su -c "$CONFDIR/bin/startup.sh" $CONFUSR
